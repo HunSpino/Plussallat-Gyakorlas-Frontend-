6a7348005201b512c56b406f6489118411776a7c
HunSpino <tamasleventelaszlo99@gmail.com>
2022-03-22 16:38:16 +0100

    az adatok megjelennek

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,35 @@
 <template>
   <div>
     <h1>Plüssök</h1>
-
+    <table>
+      <thead>
+        <tr>
+          <th>Azonsító</th>
+          <th>Plüss Név</th>
+          <th>Műveletek</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="pluss in plusses" v-bind:key="pluss.id">
+          <td>{{ pluss.id }}</td>
+          <td>{{ pluss.name }}</td>
+          <td>
+            <button>Delete</button>
+            <button>Edit</button>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="hidden" v-model="pluss.id">
+          </td>
+          <td>
+            <input type="text" v-model="pluss.name">
+          </td>
+          
+        </tr>
+      </tbody>
+    </table>
+    <button @click="loadData">Adatok betöltése</button>
   </div>
 </template>
 
@@ -11,18 +39,38 @@
 export default {
   name: 'App',
   components: {
-    
+  },
+  data(){
+    return{
+      mod_new: true, 
+      saving: false,
+      pluss: {
+        id: null,
+        name: ''
+      },
+      plusses: []
+    }
+  },
+  methods: {
+    async loadData () {
+     let Response = await fetch('http://127.0.0.1:8000/api/plusses')
+     let data = await Response.json()
+     this.plusses = data
+    },
+  },
+  mounted() {
+    this.loadData()
   }
 }
 </script>
 
 <style>
-#app {
+/*#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+}*/
 </style>
